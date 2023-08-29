ca64c8bed81e00679d145c2fa683ffd6a6676935
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-29 21:14:37 +0530

    sdsd

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,48 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+import axios from "axios";
+import 'regenerator-runtime/runtime'
 
 const App = () => {
+  let [error,Seterror]=useState('')
+  let [data,Setdata]=useState('')
+  let [movies,Setmovies]=useState([])
+  async function getdata(){
+    try{
+      let res=  await axios.get(`http://www.omdbapi.com/?s=${data}&apikey=105c5568`)
+    console.log(res.data)
+    if(res.data.Response=='True'){
+      Setmovies(res.data.Search)
+      Seterror('')
+    }
+    else{
+      Seterror('Invalid movie name. Please try again.')
+      Setmovies([])
+    }
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Search Movie</h1>
+      <input type='text' onChange={(event)=>{
+        Setdata(event.target.value)
+        Seterror("")
+      }}></input>
+      <button onClick={getdata}></button>
+      {error && <div>Movie Not found</div>}
+      { movies && <div> {movies.map(element=>{
+          return (
+            <li>
+              <div>{element.Title}</div>
+              <img src={element.Poster}></img>
+            </li>
+          )
+        })}
+        </div>}
     </div>
   )
 }
